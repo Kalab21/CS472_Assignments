6d094c701f2845b6055612b61d5ae32bc085737f
kalla <kkebed21@gmail.com>
2022-11-17 00:19:12 -0600

    Assignment12 Commit

---
+++ assignment12/my_math.js
@@ -0,0 +1,13 @@
+exports.add = function(x, y){
+    return x + y;
+}
+module.exports.subtract = function(x, y){
+    return x - y;
+}
+module.exports.multiply = function(x, y){
+    return x * y;
+}
+module.exports.divide = function(x, y){
+    return x / y;
+}
+module.exports.pi = 3.14;
---
+++ assignment12/use_car_module.js
@@ -0,0 +1,5 @@
+const car = require('./my_car');
+car.drive();
+car.turn( 45 +" degrees");
+car.turn(-35+" dsrgreees");
+car.break();
